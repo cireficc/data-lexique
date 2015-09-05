ce18299ee3358fb26ab85db49522c1477fd2e936
Chris Cirefice <cireficc@mail.gvsu.edu>
2015-09-05 16:34:35 -0400

    Remove unused code from csv2sqlite and nd show progress. Rename Lexique column to prevent duplicates during Ruby to_sym conversion

--- csv2sqlite.rb
+++ csv2sqlite.rb
@@ -17,44 +17,50 @@ end
 OPTIONS = Trollop::options do
   banner <<-EOS
 Usage:
-	csv2sqlite [options] TABLENAME.csv [...]
+	csv2sqlite [options] FILE_TO_IMPORT.csv TABLENAME [...]
 
 where [options] are:
 EOS
-  opt :irb_console,  "Open an IRB session after loading FILES into an in-memory DB"
-  opt :sqlite_console,  "Execute 'sqlite3 FILENAME.db' afterwards"
   opt :output,  "FILENAME.db where to save the sqlite database", :type => :string
 end
 
 def getDatabase(filename)
   puts "Connecting to sqlite://#{filename}"
   database = Sequel.sqlite(filename)
-  # database.test_connection # saves blank file
   return database
 end
 
-def populateTableFromCSV(database,filename)
+def populateTableFromCSV(database, file, tablename)
   options = { :headers    => true,
               :header_converters => :symbol,
-              :converters => :all  }
-  data = CSV.table(filename, options)
+              :converters => :all,
+			  :col_sep => "\t" }
+  data = CSV.table(file, options)
+  puts "CSV table is now in memory"
   headers = data.headers
-  tablename = File.basename(filename, '.csv').gsub(/[^0-9a-zA-Z_]/,'_').to_sym
 
-  puts "Dropping and re-creating table #{tablename}"
+  puts "Dropping and re-creating table: #{tablename}"
   DB.drop_table? tablename
   DB.create_table tablename do
     # see http://sequel.rubyforge.org/rdoc/files/doc/schema_modification_rdoc.html
     # primary_key :id
     # Float :price
-    data.by_col!.each do |columnName,rows|
+	puts "Processing column classes for table: #{tablename}"
+    data.by_col!.each do |columnName, rows|
+	  puts "Getting common class for #{columnName}"
       columnType = getCommonClass(rows) || String
       column columnName, columnType
     end
   end
-  data.by_row!.each do |row|
+  puts "Inserting data into table: #{tablename}; #{data.length} rows"
+  data.by_row!.each_with_index do |row, i|
+	if (i % 1000 == 0)
+	  percent_complete = ((i.to_f / data.length) * 100).round(2)
+	  puts "#{percent_complete}% completed"
+	end
     database[tablename].insert(row.to_hash)
   end
+  puts "Dtaa import complete"
 end
 
 # 
@@ -68,50 +74,6 @@ def getCommonClass(rows)
   return rows.inject(rows[0].class) { |klass, el| break if klass != el.class ; klass }
 end
 
-
-def launchConsole(database)
-  require 'irb'
-  require 'pp'
-  require 'yaml'
-
-  puts "Launching IRB Console.\n\n"
-  puts "You can now interact with the database via DB. Examples:"
-  puts "  DB.tables #=> SHOW tables"
-  puts "  ds = DB[:posts] #=> SELECT * FROM posts"
-  puts "  ds = DB[:posts].where(:id => 1) #==> SELECT * FROM posts WHERE id => 1"
-  puts "  puts DB[:posts].all ; nil #=> executes query, pretty prints results" 
-  puts ""
-  puts "See http://sequel.rubyforge.org/rdoc/files/doc/dataset_basics_rdoc.html"
-  puts "To launch sqlite3 console, type 'sqlite3'"
-  puts ""
-
-  puts "Available tables: "
-  database.tables.each do |table|
-    puts "  DB[:#{table.to_s}] - #{DB[table].count.to_s} records"
-  end
-  puts ""
-
-  IRB.start
-  catch :IRB_EXIT do
-    # IRB.start should trap this but doesn't
-    exit
-  end
-end
-
-def sqlite3()
-  launchSqliteConsole()
-end
-
-def launchSqliteConsole()
-  File.exists?(DB_PATH) or Trollop.die "Unable to launch sqlite3; invalid file: #{DB_PATH}" 
-  puts "Launching 'sqlite3 #{DB_PATH}'. Table schema:\n"
-  # NB: Using Kernel.system instead of Kernel.exec to allow Tempfile cleanup
-  system("sqlite3 #{DB_PATH} '.schema'")
-  puts ""
-  system("sqlite3 #{DB_PATH}")
-  exit
-end
-
 if OPTIONS[:output]
   DB_PATH = OPTIONS[:output]
 else
@@ -121,13 +83,11 @@ end
 
 DB = getDatabase(DB_PATH)
 
-Trollop.die "Missing CSV file argument(s)" unless ARGV.count > 0
+Trollop.die "Missing CSV file and table name arguments" unless ARGV.count > 1
 until ARGV.empty? do 
   file = ARGV.shift
-  File.exists?(file) or Trollop.die "Invalid file: #{file}" 
-  puts "Parsing file #{file}"
-  populateTableFromCSV(DB, file)
+  File.exists?(file) or Trollop.die "Invalid file: #{file}"
+  tablename = ARGV.shift.to_sym
+  puts "Parsing file #{file} into table #{tablename}"
+  populateTableFromCSV(DB, file, tablename)
 end
-
-launchSqliteConsole() if OPTIONS[:sqlite_console] 
-launchConsole(DB) if OPTIONS[:irb_console] || ! OPTIONS[:output]
